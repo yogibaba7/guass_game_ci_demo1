e0545258fcc90b52628b3599966fdb1c2b2fee6b
yogibaba7 <yogeshchouhan263@gmail.com>
2025-04-15 19:10:03 +0530

    improve code quality and add multiple os for job

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # guass game
 import random
 
+
 def generate_random_number():
-    number = random.randint(1,10)
+    number = random.randint(1, 10)
     return number
+
 
 def guess_number():
     guass_number = int(input('guass a number in range 1 to 10.....'))
     return guass_number
+
 
 def result():
     hide_number = generate_random_number()
@@ -17,10 +20,7 @@ def result():
         return 'You Won'
     else:
         return f"\nOops!, You Lose. \nThe Hidden Number is {hide_number}\n"
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     print(result())
-
-
-
-
